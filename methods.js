ad464fcb5020b56a4a66e8682d16b63d54301eab
Jashawnt Chohan <jchohan142@gmail.com>
2024-08-08 02:20:45 -0700

    first commit

---
+++ methods.js
@@ -0,0 +1,44 @@
+// // console.log("hello world");
+
+
+
+// // push method
+// Fruits.push("orange")
+// // console.log(Fruits);
+
+// // pop method
+// Fruits.pop("orange")
+// // console.log(Fruits);
+
+
+// Fruits.shift(Fruits)
+// // console.log(Fruits);
+
+// Fruits.unshift("orange")
+// // console.log(Fruits);
+
+
+// Fruits.toString(Fruits)
+// // console.log(Fruits);
+
+// let fruit = Fruits[3];
+// // console.log(fruit);
+
+
+
+// Fruits.join(Fruits)
+// // console.log(Fruits);
+
+// let Fruits2 = ["orange","kiwi"]
+
+// let Fruits3 = Fruits1.concat(Fruits2)
+
+// Fruits.copyWithin(2,0)
+
+
+// Fruits.splice(2,0,"lemon","ginger")
+
+
+let Fruits = ["mango", "apple", "banana", "pineapple"]
+let result =Fruits.slice(0,1)
+console.log(result);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,25 @@
+// console.log("hello world");
+
+
+
+// // push method
+// Fruits.push("orange")
+// // console.log(Fruits);
+
+// // pop method
+// Fruits.pop("orange")
+// // console.log(Fruits);
+
+
+// Fruits.shift(Fruits)
+// // console.log(Fruits);
+
+// Fruits.unshift("orange")
+// // console.log(Fruits);
+
+
+// Fruits.toString(Fruits)
+// // console.log(Fruits);
+
+// let fruit = Fruits[3];
+// // console.log(fruit);
